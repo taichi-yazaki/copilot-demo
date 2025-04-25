99928108898a0db6d03e8e6d86c7227429842587
“t-yazaki” <taichi.yazaki@type.jp>
2025-04-25 17:45:29 +0900

    sendEmail

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async sendBulkEmails(emails: string[]) {
+    emails.forEach((email) => {
+      this.sendEmail(email);
+    });
+    console.log('All emails sent');
+  }
+
+  async sendEmail(email: string) {
+    await new Promise((r) => setTimeout(r, 100));
+    console.log(`Email sent to ${email}`);
+  }
 }
