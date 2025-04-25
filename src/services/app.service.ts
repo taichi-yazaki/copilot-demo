ca0fc01d38a78332671e2551fafbe0a38f2618ef
“t-yazaki” <taichi.yazaki@type.jp>
2025-04-25 17:26:45 +0900

    add src/services/app.service.ts

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  sumUntil(): number {
+    let sum = 0;
+    let i = 0;
+    while (i < 5) {
+      sum += i;
+    }
+    return sum;
+  }
 }
