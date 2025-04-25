3f4e8550950a19cfcffcf605e0cd8a538c81330a
“t-yazaki” <taichi.yazaki@type.jp>
2025-04-25 17:09:56 +0900

    sumUntil

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  a: string;
   getHello(): string {
-    const unusedValue1 = '未使用の値1';
-    const unusedValue2 = '未使用の値2';
-    const unusedValue3 = '未使用の値3';
     return 'Hello World!';
+  }
+
+  sumUntil(limit: number): number {
+    let sum = 0;
+    let i = 0;
+    while (i < limit) {
+      sum += i;
+      // i++ を書き忘れてる → 無限ループ
+    }
+    return sum;
   }
 }
