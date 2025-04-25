f95fd3b4e90c81e04efee82c391a1c0b6853175b
“t-yazaki” <taichi.yazaki@type.jp>
2025-04-25 16:44:08 +0900

    undefined value

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  // 明示的に undefined を返すプロパティ
+  undefinedValue: string | undefined;
+
+  // undefined を返すメソッド
+  getUndefined(): string | undefined {
+    return undefined;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
